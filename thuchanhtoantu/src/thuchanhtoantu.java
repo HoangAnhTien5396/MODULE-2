e614dabafc58753ad28276d30b0bae2f050102bc
HoangTien <hoanganhtien53966@gmail.com>
2021-01-08 14:33:49 +0700

    thuchanhtoantu

---
+++ thuchanhtoantu/src/thuchanhtoantu.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class thuchanhtoantu {
+    public static void main(String[] args) {
+        float width;
+        float height;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println();
+        width = scanner.nextFloat("Enter width :");
+        System.out.println();
+        height = scanner.nextFloat("Enter height : ");
+
+
+    }
+}
