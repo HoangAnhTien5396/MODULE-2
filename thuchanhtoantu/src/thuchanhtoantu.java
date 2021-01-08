303da2f02fb76864fbb842983addcf0c82c74378
HoangTien <hoanganhtien53966@gmail.com>
2021-01-08 14:38:02 +0700

    thuchanhtoantu

--- thuchanhtoantu/src/thuchanhtoantu.java
+++ thuchanhtoantu/src/thuchanhtoantu.java
@@ -5,10 +5,12 @@ public class thuchanhtoantu {
         float width;
         float height;
         Scanner scanner = new Scanner(System.in);
-        System.out.println();
-        width = scanner.nextFloat("Enter width :");
-        System.out.println();
-        height = scanner.nextFloat("Enter height : ");
+        System.out.println("Enter width :");
+        width = scanner.nextFloat();
+        System.out.println("Enter height :");
+        height = scanner.nextFloat();
+        float area = width * height;
 
+        System.out.println("Area is: " + area);
     }
 }
